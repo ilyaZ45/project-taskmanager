9b7567d9fb54ccb700aed004011c4abcfada7e5e
ilyaZ45 <111ilyuha@mail.ru>
2021-04-29 22:28:11 +0300

    Почти всё

---
+++ src/components/task-edit.js
@@ -0,0 +1,94 @@
+import {
+    COLORS, DAYS, MONTH_NAMES
+}
+from "../const.js";
+import {
+    formatTime
+}
+from "../utils";
+const createColorsMarkup = (colors, curentColor) => {
+    return colors.map((color, index) => {
+        return (`<input
+                type="radio"
+                id="color-${color}-${index}"
+                class="card__color-input card__color-input--${color} visually-hidden"
+                name="color"
+                value="${color}"
+                ${curentColor === color ? `checked` : ``}
+              />
+              <label
+                for="color-${color}--${index}"
+                class="card__color card__color--${color}"
+                >${color}</label
+              >`);
+    }).join(`\n`);
+};
+const createRepeatingDaysMarkup = (days, repeatingDays) => {
+    return days.map((day, index) => {
+        const isChecked = repeatingDays[day];
+        return (`<input
+                    class="visually-hidden card__repeat-day-input"
+                    type="checkbox"
+                    id="repeat-${day}-${index}"
+                    name="repeat"
+                    value="${day}"
+                    ${isChecked ? `checked` : ``}
+                  />
+                  <label class="card__repeat-day" for="repeat-${day}-${index}"
+                    >${day}}</label
+                  >`);
+    }).join(`\n`);
+    };
+export const createTaskEditTemplate = (task) => {
+        const {
+            description, dueDate, color, repeatingDays
+        } = task;
+        const isExpired = dueDate instanceof Date && dueDate < Date.now();
+        const isDateShowing = !!dueDate;
+        const date = isDateShowing ? `${dueDate.getDate()}
+        ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
+        const time = isDateShowing ? formatTime(dueDate) : ``;
+        const isRepeatingTask = Object.values(repeatingDays).some(Boolean);
+        const repeatClass = isRepeatingTask ? `card--repeat` : ``;
+        const deadlineClass = isExpired ? `card--deadline` : ``;
+        const colorsMarkup = createColorsMarkup(COLORS, color);
+        const repeatingDaysMarkup = createRepeatingDaysMarkup(DAYS, repeatingDays);
+
+        return (`<article class="card card--edit card--${color} ${repeatClass} ${deadlineClass}">
+    <form class="card__form" method="get">
+      <div class="card__inner">
+        <div class="card__color-bar">
+          <svg width="100%" height="10">
+            <use xlink:href="#wave"></use>
+          </svg>
+        </div>
+        <div class="card__textarea-wrap">
+          <label>
+            <textarea
+              class="card__text"
+              placeholder="Start typing your text here..."
+              name="text"
+            >${description}</textarea>
+          </label>
+        </div>
+        <div class="card__settings">
+          <div class="card__details">
+            <div class="card__dates">
+              <button class="card__date-deadline-toggle" type="button">
+                date: <span class="card__date-status">${isDateShowing ? `yes` : `no`}</span>
+              </button>
+             ${
+            isDateShowing ?
+              `<fieldset class = "card__date-deadline"
+            disabled> <label class = "card__input-deadline-wrap"> <input class = "card__date"
+            type="text"
+            placeholder=""
+            name="date"
+            value="${date} ${time}"/>
+            </label> </fieldset>`: ``
+        } <button class = "card__repeat-toggle"
+        type = "button" > repeat: <span class = "card__repeat-status"> no </span> </button> <fieldset class = "card__repeat-days"
+        disabled > <div class = "card__repeat-days-inner">${repeatingDaysMarkup} </div> </fieldset> </div> </div> <div class = "card__colors-inner" > <h3 class = "card__colors-title"> Color </h3> <div class = "card__colors-wrap">${colorsMarkup} </div> </div> </div> <div class = "card__status-btns"> <button class = "card__save"
+        type = "submit"> save </button> <button class = "card__delete"
+        type = "button"> delete </button> </div> </div> </form> </article>`);
+};
---
+++ src/utils.js
@@ -0,0 +1,17 @@
+const castTimeFormat = (value) => {
+    return value < 10 ? `0${value}` : String(value);
+}
+
+
+export const formatTime = (date) => {
+  const hours = castTimeFormat(date.getHours() % 12);
+  const minutes = castTimeFormat(date.getMinutes());
+
+  return `${hours} : ${minutes}`;
+
+  //return moment(date).format(`hh:mm`);
+};
+
+export const formatDate = (date) => {
+  return moment(date).format(`DD MMMM`);
+};
